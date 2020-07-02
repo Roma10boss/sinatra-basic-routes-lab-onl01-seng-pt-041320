a04158356e5db98227553e2be02ba06c1ac7210a
Romario Gustave <romagustave10@gmail.com>
2020-07-02 20:52:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,9 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-  "My name is The Terminator"
+  "My name is The Terminator!"
 end
 
+get '/' do
+  "My hometown is Skynet!"
+end
 
+get '/' do
+  "My favorite song is beat it!"
+end
   
 end
